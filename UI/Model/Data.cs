a107dcda8859c1af4d4b7dc3dcbef41f83605b02
ykbts <102664054+ykbts@users.noreply.github.com>
2023-02-24 16:53:51 +0200

    WEB

---
+++ UI/Model/Data.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace  UI.Model
+{
+    public class Data
+    {
+        public string end_cursor { get; set; }
+        public Collector[] collector { get; set; }
+
+    }
+    public class Collector
+    {
+        public string username { get; set; }
+        public string id { get; set; }
+
+
+    }
+   
+}
