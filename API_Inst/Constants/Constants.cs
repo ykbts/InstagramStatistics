9f6c0ce08da4a4d047b506dfc8a105d85bd6826a
ykbts <102664054+ykbts@users.noreply.github.com>
2023-02-24 16:51:53 +0200

    Api Changes

--- API_Inst/Constants/Constants.cs
+++ API_Inst/Constants/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public static string address = "rocketapi-for-instagram.p.rapidapi.com";
-        public static string apikey = "424c2e56b8msh041bd3a2c5daa59p1f46c7jsn3c8210d6f55c";
+        public static string apikey = "dadc872957msh16330dd69a2d94ep1e2b26jsn2b09f504d625";
 
         public static string address_data = "instagram-data1.p.rapidapi.com";
         public static string apikey_data = "dadc872957msh16330dd69a2d94ep1e2b26jsn2b09f504d625";
